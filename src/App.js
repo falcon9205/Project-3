2872c2c0396c22f475b043b5e922a161ad69a635
Himanshu Maurya <mauryahimanshu567@gmail.com>
2024-06-09 12:17:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+// import './App.css';
+// import Loading from './Components/Loading/Loading';
+// function App() {
+//   return (
+//    <>
+//    <div>
+//    <Loading
+//     color="#36d7b7"
+//    />
+//    </div>
+    
+//    </> 
+//   )
+// }
+
+// export default App;
+
+import { useState, CSSProperties } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
+
+const override: CSSProperties = {
+  display: "block",
+  margin: "0 auto",
+  borderColor: "red",
+};
+
+function App() {
+  let [loading, setLoading] = useState(true);
+  let [color, setColor] = useState("#36d7b7");
+
+  return (
+    <div className="sweet-loading">
+      <button onClick={() => setLoading(!loading)}>Toggle Loader</button>
+      <input value={color} onChange={(input) => setColor(input.target.value)} placeholder="Color of the loader" />
+
+      <ClipLoader
+        color={color}
+        loading={loading}
+        cssOverride={override}
+        size={150}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Loading/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import  ClimbingBoxLoader  from 'react-spinners/ClimbingBoxLoader'
+const Loading = () => {
+  return (
+    <div>
+      <ClimbingBoxLoader/>
+    </div>
+  )
+}
+
+export default Loading
